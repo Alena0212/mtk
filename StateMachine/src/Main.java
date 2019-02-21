9e500e95f5ae70ab5222572c27bcf9b33bba5ee0
Алена Бастрыкина <a.bastrykina@g.nsu.ru>
2019-02-21 15:10:32 +0700

    Added main

--- StateMachine/src/Main.java
+++ StateMachine/src/Main.java
@@ -1,3 +1,20 @@
-public class Main { //TODO: add main
+import Exceptions.InputFileException;
+import Exceptions.NotValidSequenceException;
+import Exceptions.StateMachineFileException;
 
+public class Main { //TODO: add main
+    public static void main(String[] args) {
+        if (args.length < 1) {
+            System.out.println("Usage: <MachineInfoFile> <InputFile>");
+        }
+        try {
+            StateMachine stateMachine = new StateMachine(args[0]);
+            stateMachine.validateSequence(args[1]);
+            System.out.println("Input sequence is valid!");
+        } catch (StateMachineFileException | InputFileException e) {
+            System.err.println(e.getMessage());
+        } catch (NotValidSequenceException e) {
+            System.out.println("Input sequence is not valid for this machine: " + e.getMessage());
+        }
+    }
 }
--- StateMachine/src/StateMachine.java
+++ StateMachine/src/StateMachine.java
@@ -14,23 +14,23 @@ public class StateMachine {
     private Integer currentState = 0;
     public StateMachine(String filePath) throws StateMachineFileException {
         try {
-            parseMachineInfo(filePath);
+            buildMachine(filePath);
         } catch (IOException | NumberFormatException e) {
             throw new StateMachineFileException( "Bad machine info file: "+ e.getMessage());
         }
     }
 
-    private void parseMachineInfo(String machineInfoFilePath) throws IOException {
+    private void buildMachine(String machineInfoFilePath) throws IOException {
         try (Scanner sc = new Scanner(new File(machineInfoFilePath))) {
             finalStates = Arrays.stream(sc.nextLine().split(" ")).map((s)->Integer.parseInt(s)).collect(Collectors.toSet());
             while (sc.hasNextLine()) {
                 String[] data = sc.nextLine().split(" ");
                 int q1 = Integer.parseInt(data[0]), q2 = Integer.parseInt(data[2]);
-                if (!transitions.containsValue(q1)) {
+                if (!transitions.containsKey(q1)) {
                     transitions.put(q1, new HashMap<>());
                     stateCount++;
                 }
-                transitions.get(q1).put(data[0].charAt(0), q2);
+                transitions.get(q1).put(data[1].charAt(0), q2);
             }
         }
     }
@@ -42,10 +42,10 @@ public class StateMachine {
             while ((read = reader.read()) > 0) {
                 Character label = (char) read;
                 currentState = transitions.get(currentState).get(label);
-                if (currentState == null) throw new NotValidSequenceException("Character " + label.toString() + " doesn't belong to the alphabet");
+                if (currentState == null) throw new NotValidSequenceException("character '" + label.toString() + "' doesn't belong to the alphabet");
             }
         }
-        if (currentState != 0) throw new NotValidSequenceException("Sequence in the input file " + " doesn't belong to the language of the state machine");
+        if (currentState != 0) throw new NotValidSequenceException("sequence in the input file " + " doesn't belong to the language of the state machine");
     }
 
     public void validateSequence(String filePath) throws InputFileException, NotValidSequenceException {
